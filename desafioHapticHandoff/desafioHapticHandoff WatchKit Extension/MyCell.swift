9a927c0cf652aa63001242ae3f7f47e9f9287cab
Isaias Melo <ifm394@gmail.com>
2017-09-13 15:58:31 -0300

    Iniciando implentacao da tabela de haptics no watch

---
+++ desafioHapticHandoff/desafioHapticHandoff WatchKit Extension/MyCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCell.swift
+//  desafioHapticHandoff
+//
+//  Created by Isaias Fernandes on 13/09/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import WatchKit
+class MyCell : NSObject {
+    @IBOutlet var hapticTypeLabel: WKInterfaceLabel!
+}
