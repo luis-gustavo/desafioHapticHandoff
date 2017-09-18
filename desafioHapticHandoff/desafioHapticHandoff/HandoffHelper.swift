04cc221146e35615d5f6dafd4233cbb4bba6f673
krevi <krevi2796@gmail.com>
2017-09-18 17:18:51 -0300

    Configuring the handoff

---
+++ desafioHapticHandoff/desafioHapticHandoff/HandoffHelper.swift
@@ -0,0 +1,15 @@
+//
+//  HandoffHelper.swift
+//  desafioHapticHandoff
+//
+//  Created by Victor Oliveira Kreniski on 18/09/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
+
+struct HandoffHelper {
+    enum ActivityType: String {
+        case opcaoSelecionada = "br.com.bepid.opcaoSelecionada"
+    }
+}
